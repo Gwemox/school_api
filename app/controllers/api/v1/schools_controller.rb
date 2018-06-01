e56e40cde62e177b6f821b3280abb55856c7515d
Titi <thibault.buathier@k-net.pro>
2018-06-01 16:40:59 +0200

    add documentation (apipie)

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::SchoolsController < Api::ApiController
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENERATING NEXT TIME
+  api :GET, '/v1/schools', 'List schools'
+  error code: 401
   def index
     render json: {success: true}
   end
